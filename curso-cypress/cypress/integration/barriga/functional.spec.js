2652f9843e0163b90ee2a7b1e3daf2c72afefd3a
GabrielA <gabrielathalitasv@gmail.com>
2021-02-05 12:52:29 -0300

    chore: criando locators do tipo função

--- curso-cypress/cypress/integration/barriga/functional.spec.js
+++ curso-cypress/cypress/integration/barriga/functional.spec.js
@@ -23,7 +23,7 @@ describe('Should test at a functional level', () => {
 
     it('Should update an account', () => {
         cy.acessarMenuConta();
-        cy.xpath(loc.CONTAS.XP_BTN_ALT).click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALT('Conta de teste')).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
             .type('Conta alterada')
@@ -42,8 +42,15 @@ describe('Should test at a functional level', () => {
         cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Interessado')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta alterada')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click();
         cy.get(loc.MSG).should('contain', 'sucesso')
-        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist')
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc', '123')).should('exist')
+    })
+
+    it('Should get balance', () => {
+        cy.get(loc.MENU.HOME).click();
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta alterada')).should('contain', '123,00')
     })
 })
--- curso-cypress/cypress/support/locators.js
+++ curso-cypress/cypress/support/locators.js
@@ -5,25 +5,31 @@ const locators = {
         BTN_LOGIN: '.btn'
     },
     MENU: {
+        HOME: '[data-test=menu-home]',
         SETTINGS: '[data-test=menu-settings]',
         CONTAS: '[href="/contas"]',
         RESET: '[href="/reset"]',
-        MOVIMENTACAO: '[data-test=menu-movimentacao]'
+        MOVIMENTACAO: '[data-test=menu-movimentacao]',
     },
     CONTAS: {
         NOME: '[data-test=nome]',
         BTN_SALVAR: '.btn',
-        XP_BTN_ALT: "//table//td[contains(., 'Conta de teste')]/..//i[@class='far fa-edit']"
+        FN_XP_BTN_ALT: (conta) =>  `//table//td[contains(., '${conta}')]/..//i[@class='far fa-edit']`
     },
     MOVIMENTACAO: {
         DESCRICAO: '[data-test=descricao]',
         VALOR: '[data-test=valor]',
         INTERESSADO: '[data-test=envolvido]',
-        BTN_SALVAR: '.btn-primary' 
+        STATUS: '[data-test=status]',
+        BTN_SALVAR: '.btn-primary',
+        CONTA: '[data-test=conta]' 
     },
     EXTRATO: {
         LINHAS: '.list-group > li',
-        XP_BUSCA_ELEMENTO: "//span[contains(., 'Desc')]/following-sibling::small[contains(., '123')]"
+        FN_XP_BUSCA_ELEMENTO: (desc, valor) => `//span[contains(., '${desc}')]/following-sibling::small[contains(., '${valor}')]`
+    },
+    SALDO: {
+        FN_XP_SALDO_CONTA: (nome) => `//td[contains(., '${nome}')]/../td[2]`
     },
     MSG: '.toast-message',
 }
